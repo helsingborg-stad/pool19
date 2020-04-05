128fa7eb42c4712e011b7e5660a793b21f932931
nRamstedt <nikolas.ramstedt@gmail.com>
2020-04-05 15:54:04 +0200

    Fetch list items & display object as json string

--- client/src/sections/List.js
+++ client/src/sections/List.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const List = () => (
-  <div className="section section--list" id="list">
-    <div className="u-text-center">
-      <h2>Here are all the companies that need support</h2>
-      <p className="large">
-        Higher ranking on the list means the more critical the need for support is.
-      </p>
+const List = () => {
+  const [listItems, setListItems] = useState([]);
+
+  useEffect(() => {
+    axios('/api/v1/spreadsheet').then(result => {
+      setListItems(result.data);
+    });
+  }, []);
+
+  return (
+    <div className="section section--list" id="list">
+      <div className="u-text-center">
+        <h2>Here are all the companies that need support</h2>
+        <p className="large">
+          Higher ranking on the list means the more critical the need for support is.
+        </p>
+      </div>
+      <div>
+        {listItems && listItems.length > 0
+          ? listItems.map((item, index) => <li key={index}>{JSON.stringify(item)}</li>)
+          : null}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default List;
