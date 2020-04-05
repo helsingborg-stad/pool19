3e1c2eb2b28e8f5493467f508729b51a4c21fe94
nRamstedt <nikolas.ramstedt@gmail.com>
2020-04-05 17:18:46 +0200

    add header item

--- client/src/sections/List.js
+++ client/src/sections/List.js
@@ -2,26 +2,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
-const ListItem = props => {
-  const { name, need, orgNumber, city, email, grade, type } = props;
+const HeaderItem = props => {
+  const { title } = props;
 
   return (
     <Grid justify="space-between" container>
       <Grid item xs={12} md>
-        <div className="list-item">
+        <div className="">
           <div className="list-item__heading">
-            <h3>{name}</h3>
-          </div>
-          <div className="list-item__content">
-            <span>{need}</span>
-          </div>
-          <div className="list-item__meta">
-            <span>{`${orgNumber}, ${city}`}</span>
+            <h2>{title}</h2>
           </div>
         </div>
       </Grid>
-      <Grid item xs={12} md className="u-text-right"><a href={`mailto: ${email}`}>Give support ❤️</a></Grid>
     </Grid>
+  );
+};
+
+const ListItem = props => {
+  const { name, need, orgNumber, city, email, grade, type } = props;
+
+  return (
+        <Grid justify="space-between" container>
+          <Grid item xs={12} md>
+              <div className="list-item__heading">
+                <h3>{name}</h3>
+              </div>
+              <div className="list-item__content">
+                <span>{need}</span>
+              </div>
+              <div className="list-item__meta">
+                <span>{`${orgNumber}, ${city}`}</span>
+              </div>
+          </Grid>
+          <Grid item xs={12} md className="u-text-right"><a href={`mailto: ${email}`}>Give support ❤️</a></Grid>
+        </Grid>
   );
 };
 
@@ -43,9 +57,13 @@ const List = () => {
             Higher ranking on the list means the more critical the need for support is.
           </p>
         </div>
+
         <ul className="unlist">
+          <li className="list-item list-item--header">
+            <HeaderItem title={"Company Name"} />
+          </li>
           {listItems && listItems.length > 0
-            ? listItems.map((item, index) => <li key={index}>
+            ? listItems.map((item, index) => <li className="list-item" key={index}>
               <ListItem name={item.BusinessName} need={item.Need} orgNumber={item.OrgNr} city={item.Locality} grade={item.grave} email={item.Email} />
             </li>)
             : null}
