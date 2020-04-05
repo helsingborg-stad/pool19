07d8c859f4e0636007f9b22809bcabe8dcdce7ec
nRamstedt <nikolas.ramstedt@gmail.com>
2020-04-05 11:51:18 +0200

    add smoothscroll in css & add polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import smoothscroll from 'smoothscroll-polyfill';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+smoothscroll.polyfill();
 ReactDOM.render(
   <React.StrictMode>
     <App />
