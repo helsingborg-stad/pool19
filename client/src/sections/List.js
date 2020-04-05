37f739d992132bedb68190b835722239705f9586
nRamstedt <nikolas.ramstedt@gmail.com>
2020-04-05 16:58:07 +0200

    render listitem

--- client/src/sections/List.js
+++ client/src/sections/List.js
@@ -1,5 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+const ListItem = props => {
+  const { name, need, orgNumber, city, email, grade, type } = props;
+
+  return (
+    <Grid justify="space-between" container>
+      <Grid item xs={12} md>
+        <div className="list-item">
+          <div className="list-item__heading">
+            <h3>{name}</h3>
+          </div>
+          <div className="list-item__content">
+            <span>{need}</span>
+          </div>
+          <div className="list-item__meta">
+            <span>{`${orgNumber}, ${city}`}</span>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={12} md className="u-text-right"><a href={`mailto: ${email}`}>Give support ❤️</a></Grid>
+    </Grid>
+  );
+};
 
 const List = () => {
   const [listItems, setListItems] = useState([]);
@@ -12,16 +36,20 @@ const List = () => {
 
   return (
     <div className="section section--list" id="list">
-      <div className="u-text-center">
-        <h2>Here are all the companies that need support</h2>
-        <p className="large">
-          Higher ranking on the list means the more critical the need for support is.
-        </p>
-      </div>
-      <div>
-        {listItems && listItems.length > 0
-          ? listItems.map((item, index) => <li key={index}>{JSON.stringify(item)}</li>)
-          : null}
+      <div className="content-container">
+        <div className="u-text-center">
+          <h2>Here are all the companies that need support</h2>
+          <p className="large">
+            Higher ranking on the list means the more critical the need for support is.
+          </p>
+        </div>
+        <ul className="unlist">
+          {listItems && listItems.length > 0
+            ? listItems.map((item, index) => <li key={index}>
+              <ListItem name={item.BusinessName} need={item.Need} orgNumber={item.OrgNr} city={item.Locality} grade={item.grave} email={item.Email} />
+            </li>)
+            : null}
+        </ul>
       </div>
     </div>
   );
