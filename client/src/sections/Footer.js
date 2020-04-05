a4b98ce7a2b7819ca45323dbbb0eee1a748a657b
nRamstedt <nikolas.ramstedt@gmail.com>
2020-04-05 11:51:38 +0200

    Add content & markup without styles

--- client/src/sections/Footer.js
+++ client/src/sections/Footer.js
@@ -1,4 +1,23 @@
 import React from 'react';
 
-const Footer = () => <div>Footer section</div>;
+const Footer = () => (
+  <div>
+    <a href="mailto:hello@pool19.se">
+      <h2>hello@pool19.se</h2>
+    </a>
+    <ul>
+      <li>
+        <a href="#legal">Legal stuff</a>
+      </li>
+      <li>
+        <a href="#privacy">Privacy policy</a>
+      </li>
+      <li>
+        <a href="https://hbg.works" target="_blank" rel="noopener noreferrer">
+          Built by Hbg Works
+        </a>
+      </li>
+    </ul>
+  </div>
+);
 export default Footer;
